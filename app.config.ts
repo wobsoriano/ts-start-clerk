e1c6b134e69a93a0e27169e1253dec0da390d3ee
wobsoriano <sorianorobertc@gmail.com>
2025-04-07 13:20:13 -0700

    add netlify preset

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@tanstack/react-start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  server: {
+    preset: 'netlify'
+  },
   vite: {
     plugins: [
       tsConfigPaths({
